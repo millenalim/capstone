432213045d06ef43e2b4fc602ee34327176a311b
ZcYau <zachary.yau@gmail.com>
2023-01-16 09:19:24 -0600

    backend login

---
+++ capstone-project/capstone-project-spring/src/main/java/learn/capstone/data/AppUserRepository.java
@@ -0,0 +1,16 @@
+package learn.capstone.data;
+
+import learn.capstone.models.AppUser;
+
+import org.springframework.transaction.annotation.Transactional;
+
+public interface AppUserRepository {
+    @Transactional
+    AppUser findByUsername(String username);
+
+    @Transactional
+    AppUser create(AppUser user);
+
+    @Transactional
+    boolean update(AppUser user);
+}
---
+++ capstone-project/capstone-project-spring/src/main/java/learn/capstone/domain/ActionStatus.java
@@ -0,0 +1,8 @@
+package learn.capstone.domain;
+
+public enum ActionStatus {
+    SUCCESS,
+    INVALID,
+    DUPLICATE,
+    NOT_FOUND
+}
