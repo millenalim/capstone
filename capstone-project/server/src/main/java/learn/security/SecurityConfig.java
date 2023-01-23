937842384b4d835eea1b71867722278d5fbabee3
ZcYau <zachary.yau@gmail.com>
2023-01-23 16:20:01 -0600

    fixed username in securityconfig

--- capstone-project/server/src/main/java/learn/security/SecurityConfig.java
+++ capstone-project/server/src/main/java/learn/security/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST,"/user").hasAnyAuthority("USER")
                 .antMatchers(HttpMethod.GET,
                         "/").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/user/*").permitAll()
                 .antMatchers(HttpMethod.GET,"/users").permitAll()
 
                 .antMatchers(HttpMethod.PUT, "/create_profile/*").hasAnyAuthority("USER", "ADMIN")
